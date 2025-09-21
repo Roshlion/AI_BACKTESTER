a8fcd95010531148fa267ce1f434c97f1b4ccdf7
Roshen Jegajeevan <roshlion@gmail.com>
2025-09-21 14:30:57 -0400

    chore(types): add parquetjs-lite ambient types for Vercel build

---
+++ types/parquetjs-lite.d.ts
@@ -0,0 +1,7 @@
+﻿declare module "parquetjs-lite" {
+  export const ParquetReader: any;
+  export const ParquetWriter: any;
+  export const ParquetSchema: any;
+  const _default: any;
+  export default _default;
+}
